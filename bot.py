3e8277323f0476c4623daffadf02481dd79c424c
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-06 17:27:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,7 @@ import pyrogram
 from pykeyboard import InlineKeyboard, ReplyKeyboard
 from pyrogram import Client, filters
 from pyrogram.errors import UserNotParticipant
-from pyrogram.types import ForceReply, InlineKeyboardButton
-                            InlineKeyboardMarkup, InputTextMessageContent,
-                            KeyboardButton
+from pyrogram.types import ForceReply, InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent, KeyboardButton
 import json
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 from telegram import ParseMode
